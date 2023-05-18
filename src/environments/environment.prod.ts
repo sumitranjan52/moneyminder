a71de6e7cbc0f96bee6c2f99b8a710bf967fe1e5
Sumit Ranjan <sumitranjan52@gmail.com>
2023-05-19 00:26:23 +0530

    updated api and icon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //api: "http://localhost:9090/MoneyMinder/api/",
-  api: "https://mm-j.herokuapp.com/api/",
+  api: "http://ec2-65-2-73-242.ap-south-1.compute.amazonaws.com/moneyminder/api/",
   token: "MM-Safe-Token",
 };
